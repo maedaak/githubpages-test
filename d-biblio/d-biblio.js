45dcf26105515c80a735e946c108cd7dffaf3092
Maeda Akira <maeda.akir.72@gmail.com>
2021-05-23 07:08:24 +0900

    6th commit

--- d-biblio/d-biblio.js
+++ d-biblio/d-biblio.js
@@ -2,39 +2,39 @@ enchant();
 
 // ページが読み込まれたときに実行される関数
 window.onload = function() {
- 
+
   core = new Core(570, 570);
   core.fps = 16;
- 
+
   core.preload('moon.png');
-  
+
    core.onload = function() {
-   	   
+
    	   var bg = new Sprite(570, 570);
    	   bg.image = core.assets['moon.png'];
    	   core.rootScene.addChild(bg);
-   	   
-   	   var label1 = new Label("図書館がわかる本");
+
+   	   var label1 = new Label("図書館員のためのアプリ開発がわかる本");
    	   label1.color = '#0000FF';
    	   label1.font =  "italic bold 30px 'HG正楷書体-PRO'";
    	   label1.x = 200;
    	   label1.y = 200;
    	   core.rootScene.addChild(label1);
    	   var count = 0;
-   	   
+
    	   var label2 = new Label("前田朗著");
    	   label2.color = 'green';
    	   label2.font =  "30px 'HG正楷書体-PRO'";
    	   label2.x = 500;
    	   label2.y = 0;
    	   core.rootScene.addChild(label2);
-   	   
+
     	   var label3 = new Label("刊行予定とかなし！");
    	   label3.color = 'red';
    	   label3.font =  "35px 'HG正楷書体-PRO'";
    	   label3.x = 100;
    	   label3.y = 500;
- 
+
    	   this.addEventListener('enterframe', function() {
    	   	   if (count < 320) {
    	   	       // label1.x = (Math.sin(count / 180) * 100) + 200;
@@ -55,7 +55,7 @@ window.onload = function() {
    	   	   count ++;
    	   });
    }
-   
+
    core.start();
 
 }
